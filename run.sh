42b4f693cb6827705bda2a44fcd18d032b15146c
trxe <peixianwong@gmail.com>
2025-09-17 09:05:33 +0800

    changes to yamls and common.rs

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
-cargo run -- -c config/example -o output/ -p puckdata -r default -C
+set -e
+cargo build
+
+nohup ./target/debug/cp_demo --config ./config/ --output ./output/ --runner demo-run-once --pipeline game_info --execute > ./output/run_game_info.log 2>&1 &
+
+nohup ./target/debug/cp_demo --config ./config/ --output ./output/ --runner demo-run-loop --pipeline game_day --execute > ./output/run_game_info.log 2>&1 &
